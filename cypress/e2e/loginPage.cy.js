f04473bd1ea79cca9663dbd8c36703a6955f32d4
Guilherme Campos <157999298+guilhermecampos-dev@users.noreply.github.com>
2025-12-19 10:56:02 -0300

    refactor: organize login and inventory tests

--- cypress/e2e/loginPage.cy.js
+++ cypress/e2e/loginPage.cy.js
@@ -20,23 +20,15 @@ beforeEach(() =>{
   })
 
   it('should display inventory page after login', () =>{
-    loginPage.login('standard_user', 'secret_sauce')
-    cy.get('.title').should('have.text', 'Products')
-  })
-
-  it('should not login with valid', () =>{
+  cy.login()
+  inventoryPage.validateInventoryPage()
+})
+  it('should not login with invalid credentials', () =>{
     loginPage.login('wrong_user', 'wrong_password')
     loginPage.validateErrorMessage()
     cy.url().should('include','saucedemo.com')
   })
-it('should login with valid credentials', () =>{
-  loginPage.login('standard_user', 'secret_sauce')
-  inventoryPage.validateInventoryPage()
-})
 
-it('should display inventory page after login', () =>{
-  loginPage.login('standard_user','secret_sauce')
-  inventoryPage.validateInventoryPage()
-})
+
 
 })
